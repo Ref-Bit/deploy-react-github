0c3cb13541a19f70ec21379be2c41924b1aab132
Ref-Bit <rsv.gho@gmail.com>
2020-04-27 20:24:30 +0300

    Updates

---
+++ precache-manifest.ec62b2b9c2e8d67706c04699749839cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d54ca6906ee307f7357b716d238295c",
+    "url": "/deploy-react-github/index.html"
+  },
+  {
+    "revision": "1a391a5a7e965d4ae666",
+    "url": "/deploy-react-github/static/css/main.c503e745.chunk.css"
+  },
+  {
+    "revision": "e3372dab34ed592d1e59",
+    "url": "/deploy-react-github/static/js/2.fa07a3cb.chunk.js"
+  },
+  {
+    "revision": "1a391a5a7e965d4ae666",
+    "url": "/deploy-react-github/static/js/main.0eb3e95f.chunk.js"
+  },
+  {
+    "revision": "7d55b8ea6cc45afaa0aa",
+    "url": "/deploy-react-github/static/js/runtime~main.ea386d64.js"
+  }
+]);
